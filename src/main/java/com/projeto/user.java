cd9a2222bd14ea1505727977759b3c69e9afbeba
Nicole Francani <nfrancani@coachit.com.br>
2020-04-06 22:32:05 -0300

    Modificando o design e configurando a pagina de login e cadastro

---
+++ src/main/java/com/projeto/user.java
@@ -0,0 +1,33 @@
+package com.projeto;
+
+public class user {
+	
+	private String name;
+    private String password;
+    private boolean admin;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        admin = admin;
+    }
+
+}
